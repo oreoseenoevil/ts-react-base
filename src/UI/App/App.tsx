a14cb2e51722d6e776021a119215ae06866da18b
jessie <creators.nuke@gmail.com>
2022-04-05 08:55:26 +0800

    fixed: folder structure

---
+++ src/UI/App/App.tsx
@@ -0,0 +1,11 @@
+import styles from './App.module.scss';
+
+export const App = () => {
+  return (
+    <div className={styles.App}>
+      <header className={styles.App_header}>Hello World</header>
+    </div>
+  );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
-import './index.css';
-import App from './App';
+import { App } from 'UI/App';
+import 'Styles/main.scss';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
