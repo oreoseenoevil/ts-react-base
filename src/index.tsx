9a33c6af42ecc2b04fcda644f8c2df947ec7a336
jessie <creators.nuke@gmail.com>
2022-05-17 12:05:14 +0800

    chore(react): updated to 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import { App } from 'UI/App';
 import 'Styles/main.scss';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+
+const root = ReactDOM.createRoot(rootElement as Element);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
